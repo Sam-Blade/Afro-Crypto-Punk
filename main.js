14486758e58edc1d55da8c70646c390cfb648eb9
Blade <34589310+Sam-Blade@users.noreply.github.com>
2022-09-19 07:59:13 -0400

    Temp Website

---
+++ main.js
@@ -0,0 +1,5 @@
+alert("test code");
+console.log("Website server is temporary down. Follow us on Twitter to stay up to date")
+
+document.write('Hello');
+//Creating Temporary website until server is back up
