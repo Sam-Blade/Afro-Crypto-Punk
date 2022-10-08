1a1cb1906f60ff53ba870452c717e03cc4974a01
Blade <34589310+Sam-Blade@users.noreply.github.com>
2022-10-08 10:51:37 -0400

    Update main.js

--- main.js
+++ main.js
@@ -1,5 +1,7 @@
 alert("test code");
 console.log("Website server is temporary down. Follow us on Twitter to stay up to date")
 
-document.write('Hello');
-//Creating Temporary website until server is back up
+document.write('ws=website');
+//Creating Temporary website until server  
+//Temporary Wesbite is up!!
+//,
